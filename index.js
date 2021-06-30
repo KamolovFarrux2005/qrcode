54cf764b9fb0dde861d07760edf3b15dc78490e9
kamalov <kfarrux2005@gmail.com>
2021-06-30 08:01:35 +0500

    qrcode

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+const qr = require('qrcode');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+app.get('/' , (req , res) => {
+
+   res.render('index')
+
+});
+
+app.post('/qrcode' , (req , res) => {
+    const url = req.body.url;
+    if(url.length == 0) res.send('empty data!');
+    qr.toDataURL(url, (err, data)=>{
+        if(err) res.send('error occured');
+        res.render('qrcode', {data});
+    })
+})
+app.listen(5000, () =>  {
+    console.log(`Server started on port`);
+});
